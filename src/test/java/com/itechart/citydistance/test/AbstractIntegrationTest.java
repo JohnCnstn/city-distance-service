5a5d5852353bcb8df08152d0737412d8594757dc
Pavel Khankevich <crystalfaster@gmail.com>
2019-05-10 18:10:04 +0300

    Configure abstract integration test and neo4j initializer for tests Write openapi tests

---
+++ src/test/java/com/itechart/citydistance/test/AbstractIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.itechart.citydistance.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = {
+        Neo4jInitializer.class,
+})
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected ResourceLoader resourceLoader;
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+}
