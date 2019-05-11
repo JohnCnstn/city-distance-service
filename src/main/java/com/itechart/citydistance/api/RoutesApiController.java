dd3658a75c2da99b2584d3c123eeddae16894572
Pavel <crystalfaster@gmail.com>
2019-05-11 17:11:37 +0300

    Add paging for get routes endpoint

--- src/main/java/com/itechart/citydistance/api/RoutesApiController.java
+++ src/main/java/com/itechart/citydistance/api/RoutesApiController.java
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+
+import static com.itechart.citydistance.util.HeaderUtil.generatePaginationHeaders;
+import static com.itechart.citydistance.util.PagingUtil.toPageable;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +26,14 @@ public class RoutesApiController implements RoutesApi {
     private final RouteService routeService;
 
     @Override
-    public ResponseEntity<List<Route>> getRoutes(@NotNull @Valid String from, @NotNull @Valid String to) {
-        var routes = routeService.getRoutes(from, to);
-        return new ResponseEntity<>(routes, HttpStatus.OK);
+    public ResponseEntity<List<Route>> getRoutes(
+            @NotNull @Valid String from,
+            @NotNull @Valid String to,
+            @Min(1) @Valid Integer page,
+            @Min(1) @Max(100) @Valid Integer size) {
+        var routes = routeService.getRoutes(from, to, toPageable(page, size));
+        var headers = generatePaginationHeaders(routes);
+        return new ResponseEntity<>(routes.getContent(), headers, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/itechart/citydistance/service/impl/RouteServiceImpl.java
+++ src/main/java/com/itechart/citydistance/service/impl/RouteServiceImpl.java
@@ -4,11 +4,10 @@ import com.itechart.citydistance.generated.model.Route;
 import com.itechart.citydistance.repository.RoadRepository;
 import com.itechart.citydistance.service.RouteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.itechart.citydistance.mapper.RouteMapper.ROUTE_MAPPER;
 
@@ -20,9 +19,9 @@ public class RouteServiceImpl implements RouteService {
 
     @Override
     @Transactional(readOnly = true)
-    public List<Route> getRoutes(String from, String to) {
-        var roads = roadRepository.findRoads(from, to);
-        return roads.stream().map(ROUTE_MAPPER::toModel).collect(Collectors.toList());
+    public Page<Route> getRoutes(String from, String to, Pageable pageable) {
+        var roads = roadRepository.findRoads(from, to, pageable);
+        return roads.map(ROUTE_MAPPER::toModel);
     }
 
 }
